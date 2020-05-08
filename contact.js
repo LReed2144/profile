65304d13030dd8f8867448adbd79e343c98bcad2
Lyndsey Reed <ler2144@tc.columbia.edu>
2020-05-08 19:45:17 -0400

    changes

--- contact.js
+++ contact.js
@@ -11,21 +11,21 @@ $(document).ready(function() {
     const statusElm = $('.status')
     statusElm.empty()
 
-    if (text.length > 1) {
+    if (text.length >= 1) {
       statusElm.append('<div>Name is valid</div>')
     } else {
       event.preventDefault()
       statusElm.append('<div>Please add a name</div>')
-    };
+    }
 
-    if (email.length > 1 && email.includes('@') && email.includes(`.`)) {
+    if (email.length >= 1 && email.includes('@') && email.includes(`.`)) {
       statusElm.append('<div>Email is valid</div>')
     } else {
       event.preventDefault()
       statusElm.append('<div>Email is not valid</div>')
-    };
+    }
 
-    if (msg.length >1) {
+    if (msg.length >= 1) {
       statusElm.append('<div>Message is valid</div>')
     } else {
       event.preventDefault()
