d0497050a2f9cbed190c69562a2f11e9b0185305
Lyndsey Reed <ler2144@tc.columbia.edu>
2020-05-09 06:00:28 -0400

    changes form

--- contact.js
+++ contact.js
@@ -9,36 +9,21 @@ function validateForm() {
   }
 }
 
+function validateForm() {
+  var email = document.forms["myForm"]["femail"].value;
 
-$(document).ready(function() {
-  $('.submit').click(function(event) {
-    event.preventDefault()
+  if (email == "") {
+    alert("Email must be filled out");
+    return false;
+  }
+}
 
-    const text =  $('.text').val()
-    const email = $('.email').val()
-    const msg = $('.msg').val()
-    const statusElm = $('.status')
-    statusElm.empty()
+function validateForm() {
+  var email = document.forms["myForm"]["fmsg"].value;
 
-    if (text.length >= 1) {
-      statusElm.append('<div>Name is valid</div>')
-    } else {
-      event.preventDefault()
-      statusElm.append('<div>Please add a name</div>')
-    }
+  if (email == "") {
+    alert("Message must be filled out");
+    return false;
+  }
+}
 
-    if (email.length >= 1 && email.includes('@') && email.includes(`.`)) {
-      statusElm.append('<div>Email is valid</div>')
-    } else {
-      event.preventDefault()
-      statusElm.append('<div>Email is not valid</div>')
-    }
-
-    if (msg.length >= 1) {
-      statusElm.append('<div>Message is valid</div>')
-    } else {
-      event.preventDefault()
-      statusElm.append('<div>Please add a message</div>')
-    }
-  })
-})
